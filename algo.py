58fa5dda392d1320338705ec4d04f1e2a8cf612f
Maxime Gloesener <max.gleu@gmail.com>
2022-11-14 17:54:02 +0100

    update init solution combi linéaire f obj

---
+++ algo.py
@@ -0,0 +1,15 @@
+from utils import get_matrix, init_solution, read_data, score
+
+file_name = "Data/LAP-8-2objSOL.txt"
+nombre_objectif = 2
+
+# Lecture des données
+d = read_data(file_name, nombre_objectif)
+mx = get_matrix(file_name, nombre_objectif)
+
+sl = init_solution(mx, 8, 2, 1)
+
+for c in sl:
+    print(score(c,d,2))
+
+
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+from itertools import product
 
+# lit les données et return un dictionnaire (machine, job): [f obj1, f obj2]
 def read_data(file_name, nobj):
     with open(file_name, 'r') as fin:
         taille = int(fin.readline())
@@ -13,25 +15,52 @@ def read_data(file_name, nobj):
                 line = list(map(int,fin.readline().strip().split()))
                 for i,j in enumerate(line):
                     d[(m,i)].append(j)
-                m += 1  
-           
+                m += 1           
     return d
 
 
-# objectif = trouver la solution exacte pour chaque objectif séparément pour trouver les valeurs extrêmes
-def init_solution(file_name, nobj):
+# données sous forme matricielle pour init des solutions en faisant des combinaisons linéaires des objectifs
+def get_matrix(file_name, nobj):
     with open(file_name, 'r') as fin:
-        sols = []
+        matrices = []
         taille = int(fin.readline())
         for _ in range(nobj):
             mx = np.zeros(shape=(taille, taille))
             for j in range(taille):
                 line = np.array(list(map(int, fin.readline().strip().split())))
                 mx[j] = line 
-            
-            row_ind, col_ind = linear_sum_assignment(mx)
-            sols.append(col_ind)
-    return sols
+            matrices.append(mx)
+    return matrices
 
-sols = init_solution("Data/LAP-8-2objSOL.txt",2)
-print(sols)
+
+# objectif = trouver la solution exacte pour chaque objectif séparément pour trouver les valeurs extrêmes
+# size = coef
+# nobjectif
+def init_solution(mx, size, nobj, max_coef):
+    # génération de coefficients pour les combinaisons linéaires des fonctions objectifs
+    x = [int(i) for i in range(max_coef+1)]
+    coef = []
+    for iter in product(x,repeat = nobj):
+        if sum(iter)>0:
+            coef.append(iter)
+    
+    # multiplication des fonctions objectifs par ces coefficients et calcul du vecteur optimale pour chaque combinaison linéaire
+    sols = []
+    for c in coef:
+        m = np.zeros(shape=(size, size))
+        for i,z in enumerate(c):
+            m+=z*mx[i]
+        row_ind, col_ind = linear_sum_assignment(m)
+        sols.append(col_ind) 
+    return sols 
+
+
+# fonction qui calcule le score d'une solution
+# entrée = vecteur sol, d = dictionnaire qui stocke les données et nbre objectifs
+def score(sol, d, nobj):
+    obj = np.zeros(shape=(1,nobj))
+    for i,v in enumerate(sol):
+        obj+=d[(i,v)]      
+    return obj
+
+
