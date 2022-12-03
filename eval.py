7bfe98508c5dc55c9420e2cb592b8f7dd6e161c5
Maxime Gloesener <max.gleu@gmail.com>
2022-12-03 23:49:23 +0100

    fct eval

--- eval.py
+++ eval.py
@@ -1,23 +1,23 @@
 # Comparaison des solutions obtenus avec celles des autres étudiants / celles du prof
 from utils import hypervolume, domine
 
+# trouve le nombre de solutions dominées par une autre solution
+def number_dominated(sol1, sol2):
+    number = 0
+    for sc in sol1:
+            for sc2 in sol2:
+                # si score 1 dominé alors compteur + 1 et on passe au prochain score du jeu 1
+                if domine(sc2, sc):
+                    number+=1
+                    break 
+    return number
+
 def compare(filenameA, filenameB, reference):
     with open(filenameA, 'r') as f1, open(filenameB, 'r') as f2:
         data1 = [tuple(map(int,line.strip().split())) for line in f1]
         data2 = [tuple(map(int,line.strip().split())) for line in f2]
-        domine1 = 0
-        domine2 = 0
-        for sc in data1:
-            for sc2 in data2:
-                # si score 1 dominé alors compteur + 1 et on passe au prochain score du jeu 1
-                if domine(sc2, sc):
-                    domine1+=1
-                    break 
-        for sc in data2:
-            for sc2 in data1:
-                if domine(sc2,sc):
-                    domine2+=1
-                    break 
+        domine1 = number_dominated(data1, data2)
+        domine2 = number_dominated(data2, data1)
     print(f"Nombre de solutions dominées dans 1 par 2 = {domine1}")
     print(f"Nombre de solutions dominées dans 2 par 1 = {domine2}")
     print(f"Nombre de solutions non dominées dans 1 par 2 = {len(data1)-domine1}")
