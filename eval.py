27acc3878dfc06d7aa9e95787f5ad7e5d1b493a7
Maxime Gloesener <max.gleu@gmail.com>
2022-12-04 23:23:44 +0100

    eval

--- eval.py
+++ eval.py
@@ -6,21 +6,25 @@ from utils import hypervolume, domine
 # trouve le nombre de solutions dominées par une autre solution
 def number_dominated(sol1, sol2):
     number = 0
+    same = 0
     for sc in sol1:
         for sc2 in sol2:
+            if sc == sc2:
+                same += 1
             # si score 1 dominé alors compteur + 1 et on passe au prochain score du jeu 1
             if domine(sc2, sc):
                 number += 1
                 break
-    return number
+    return number, same
 
 
 def compare(filenameA, filenameB, reference):
     with open(filenameA, 'r') as f1, open(filenameB, 'r') as f2:
         data1 = [tuple(map(int, line.strip().split())) for line in f1]
         data2 = [tuple(map(int, line.strip().split())) for line in f2]
-        domine1 = number_dominated(data1, data2)
-        domine2 = number_dominated(data2, data1)
+        domine1, same = number_dominated(data1, data2)
+        domine2, same = number_dominated(data2, data1)
+    print(f"Nombre de solutions identiques : {same}")
     print(f"Nombre de solutions dominées dans 1 par 2 = {domine1}")
     print(f"Nombre de solutions dominées dans 2 par 1 = {domine2}")
     print(
@@ -36,5 +40,5 @@ def compare(filenameA, filenameB, reference):
     print(f"Hypervolume 2 = {hypervolume(reference, data2)}")
 
 
-ref = (900, 900)
-compare("Data/solutions/test1.txt", "Data/solutions/test2.txt", ref)
+ref = (900, 900, 900, 900)
+compare("Data/solutions/tanguySOL154.txt", "Data/solutions/LAP15-4obj.txt", ref)
